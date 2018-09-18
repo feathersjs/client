dac38fae94fbd18665539425ff3c265990196608
David Luecke <daff@neyeon.com>
2018-09-17 18:41:09 -0700

    Update all dependencies and build

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,12 @@ function createConfig (name, isProduction = false) {
     module: {
       rules: [{
         test: /\.js/,
-        exclude: /node_modules\/(?!(@feathersjs))/,
-        loader: 'babel-loader'
+        exclude: /node_modules\/(?!(@feathersjs|debug))/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
       }]
-    },
-    resolve: {
-      mainFields: ['browser', 'module', 'main']
     }
   };
 
